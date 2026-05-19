e8eba9386528c0a3109002f6a02392b36aa815a2
Christopher Harrop <35781497+christopherwharrop-noaa@users.noreply.github.com>
2026-05-19 11:25:17 -0600

    Update spack-stack to 2.1.0 and ubuntu to 26.04 (#7)
    * Update spack-stack to 2.1.0 and ubuntu to 26.04
    
    * Use generic x86_64 microarchitecture and add secrets for binary cache use
    
    * Update CI workflow to build using caches if available
    
    * Add tests for arm and cleanup
    
    * Fix incorrect message in README, don't run CI on changes to README

--- test/test_hello.sh
+++ test/test_hello.sh
@@ -2,12 +2,15 @@
 
 set -e
 
-module use /opt/spack-stack/envs/unified-env/install/modulefiles/Core
+module use /opt/spack-stack/envs/unified-env/modules/Core
+
 module load stack-gcc
 module load stack-openmpi
-module load stack-python
 
 mpif90 -o hello.exe mpi_hello.f90
-srun -N 3 --tasks-per-node=2 hello.exe | sort > hello.out
 
+srun --mpi=pmix -N 3 --tasks-per-node=2 ./hello.exe | sort > hello.out
+diff hello.out hello.baseline
+
+srun -N 3 --tasks-per-node=2 ./hello.exe | sort > hello.out
 diff hello.out hello.baseline
